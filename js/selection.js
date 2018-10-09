d5eb4f8b5d19f5b805edfa281d6005f90994ea00
cheerswineandliquors <42143722+cheerswineandliquors@users.noreply.github.com>
2018-10-08 22:14:29 -0400

    October Update

--- js/selection.js
+++ js/selection.js
@@ -1,13 +1,21 @@
 var selectionAccordionMain = document.getElementsByClassName("accordionMain");
 var i;
 var accordionMainHeight;
+var accordionNMainHeight;
 var selectionAccordionSec  = document.getElementsByClassName("accordionSec");
 var j;
+var selectionAccordionNull  = document.getElementsByClassName("accordionNull");
+var k;
 var panel;
-
+var panelN;
 function refresh(panelMain) {
 	
 	panel=panelMain;
+	
+}
+function refreshN(panelNull) {
+	
+	panelN=panelNull;
 	
 }
 for (i = 0; i < selectionAccordionMain.length; i++) 
@@ -21,6 +29,7 @@ for (i = 0; i < selectionAccordionMain.length; i++)
 			else {
 				panelMain.style.maxHeight = panelMain.scrollHeight + "px";
 				refresh(panelMain);
+			
 			} 
 		}
   );
@@ -39,8 +48,27 @@ for (j = 0; j < selectionAccordionSec.length; j++)
 			else {
 				panelSec.style.maxHeight =  panelSec.scrollHeight + "px";
 				panel.style.maxHeight = panel.scrollHeight + panelSec.scrollHeight + "px";
-
+				panelN.style.maxHeight = panelN.scrollHeight + panelSec.scrollHeight + "px";
 			} 
 		}
   );
+}
+
+for (k = 0; k < selectionAccordionNull.length; k++) 
+{
+	selectionAccordionNull[k].addEventListener("click", function() {
+		this.classList.toggle("selactive");
+		var panelNull = this.nextElementSibling;
+		if (panelNull.style.maxHeight){
+			panelNull.style.maxHeight = null;
+		} 
+		else {
+			panelNull.style.maxHeight =  panelNull.scrollHeight + "px";
+			panel.style.maxHeight = panel.scrollHeight + panelNull.scrollHeight + "px";
+		
+			refreshN(panelNull);
+			}
+		} 
+	
+);
 }
