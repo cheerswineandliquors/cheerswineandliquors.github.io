aa6111f68a07a70a14a51145bbc495224e714587
cheerswineandliquors <42143722+cheerswineandliquors@users.noreply.github.com>
2019-12-20 23:25:40 -0500

    Navbar HFX

--- js/selection.js
+++ js/selection.js
@@ -1,74 +1,23 @@
-var selectionAccordionMain = document.getElementsByClassName("accordionMain");
-var i;
-var accordionMainHeight;
-var accordionNMainHeight;
-var selectionAccordionSec  = document.getElementsByClassName("accordionSec");
-var j;
-var selectionAccordionNull  = document.getElementsByClassName("accordionNull");
-var k;
-var panel;
-var panelN;
-function refresh(panelMain) {
-	
-	panel=panelMain;
-	
-}
-function refreshN(panelNull) {
-	
-	panelN=panelNull;
-	
-}
-for (i = 0; i < selectionAccordionMain.length; i++) 
-	{
-		selectionAccordionMain[i].addEventListener("click", function() {
-			this.classList.toggle("selactive");
-			var panelMain = this.nextElementSibling;
-			if (panelMain.style.maxHeight){
-				panelMain.style.maxHeight = null;
-			} 
-			else {
-				panelMain.style.maxHeight = panelMain.scrollHeight + "px";
-				refresh(panelMain);
-			
-			} 
-		}
-  );
-}
-
-
-
-for (j = 0; j < selectionAccordionSec.length; j++) 
-	{
-		selectionAccordionSec[j].addEventListener("click", function() {
-			this.classList.toggle("selactive");
-			var panelSec = this.nextElementSibling;
-			if (panelSec.style.maxHeight){
-				panelSec.style.maxHeight = null;
-			} 
-			else {
-				panelSec.style.maxHeight =  panelSec.scrollHeight + "px";
-				panel.style.maxHeight = panel.scrollHeight + panelSec.scrollHeight + "px";
-				panelN.style.maxHeight = panelN.scrollHeight + panelSec.scrollHeight + "px";
-			} 
-		}
-  );
-}
-
-for (k = 0; k < selectionAccordionNull.length; k++) 
-{
-	selectionAccordionNull[k].addEventListener("click", function() {
-		this.classList.toggle("selactive");
-		var panelNull = this.nextElementSibling;
-		if (panelNull.style.maxHeight){
-			panelNull.style.maxHeight = null;
-		} 
-		else {
-			panelNull.style.maxHeight =  panelNull.scrollHeight + "px";
-			panel.style.maxHeight = panel.scrollHeight + panelNull.scrollHeight + "px";
-		
-			refreshN(panelNull);
-			}
-		} 
-	
-);
-}
+function selector(number) {
+	var dropdown_content = document.getElementsByClassName("dropdown-content");
+	dropdown_content[0].classList.remove('show');
+	dropdown_content[1].classList.remove('show');
+	dropdown_content[2].classList.remove('show');
+	dropdown_content[number].classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(e) {
+	if (!e.target.matches('.dropbtn')) {
+		var dropdown_content = document.getElementsByClassName("dropdown-content");
+	  	if (dropdown_content[0].classList.contains('show')) {
+			dropdown_content[0].classList.remove('show');
+	  }
+	  if (dropdown_content[1].classList.contains('show')) {
+		dropdown_content[1].classList.remove('show');
+	  }
+	  if (dropdown_content[2].classList.contains('show')) {
+		dropdown_content[2].classList.remove('show');
+	  }
+	}
+  }
